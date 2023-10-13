82ffbf4a02a4874b99153139925ffb997213b025
Polina Vigovskaya <polinavigovskaya1@gmail.com>
2023-10-13 03:12:25 +0300

    **

--- js/shop.js
+++ js/shop.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-	
+
 	$('.add-to-busket-block').hide();
-	
-	
+
+
 	$('.add-to-busket-btn').on('click', function() {
 		$('.add-to-busket-block').show();
 		$('.count-input').val("0");
 	});
-	
+
 	$('.btn-plus').on('click', function() {
-	var a; 
+	var a;
 	a= parseInt(($('.count-input').val()))+1;
 		$('.count-input').val(a);
 	});
 
 	$('.btn-minus').on('click', function() {
-	var a; 
+	var a;
 	a= parseInt(($('.count-input').val()))-1;
-	
+
 		if(a>=0){
 		$('.count-input').val(a);
 		}
@@ -25,18 +25,18 @@ $(document).ready(function() {
 			a=0;
 		}
 	});
-	
+
 	$('.btn-close').on('click', function() {
 	$('.add-to-busket-block').hide();
 	});
-	
+
 	$('.close').on('click', function() {
 	$('.add-to-busket-block').hide();
 	});
-	
+
 	let cart = new Array();
 	cart=[];
-	
+
 	$('.ok-1').on('click', function() {
 		var a = $('.count-input').val();
 		AddToCard(0, a);
@@ -45,7 +45,7 @@ $(document).ready(function() {
 		var a = $('.count-input').val();
 		AddToCard(1, a);
 	});
-		$('.ok-3').on('click', function() {
+	$('.ok-3').on('click', function() {
 		var a = $('.count-input').val();
 		AddToCard(2, a);
 	});
@@ -53,94 +53,110 @@ $(document).ready(function() {
 		var a = $('.count-input').val();
 		AddToCard(3, a);
 	});
-		$('.ok-5').on('click', function() {
+	$('.ok-5').on('click', function() {
 		var a = $('.count-input').val();
 		AddToCard(4, a);
 	});
-	
-	
+	$('.ok-6').on('click', function() {
+                var a = $('.count-input').val();
+                AddToCard(5, a);
+          });
+
 	function AddToCard(n, amount){
-		if(amount>0) 
+		if(amount>0)
 		{
-			cart[n]=amount; 
+			cart[n]=amount;
 		}
 	}
 
 	function addRow() {
 		for(let i=0; i<cart.length; i++){
-		
+
 		if(cart[i]>0){
 		/*	document.querySelector('.cart-body').insertAdjacentHTML(
 			'afterbegin',
 			`<div class="row">
 			<input readonly class="product"></input>
 			<input readonly class="amount"></input>
-			</div>`      
+			</div>`
 			) */
-	
+
 			switch(i) {
 				case 0:
-					//var t = "Product1";
+					//var t = "Dessert candle";
 					document.querySelector('.cart-body').insertAdjacentHTML(
 			'afterbegin',
 			`<div class="row">
 			<input readonly class="product0"></input>
 			<input readonly class="amount0"></input>
 			</div>`)
-			$('.product0').val("Product1");
+			$('.product0').val("Dessert candle");
 			$('.amount0').val(cart[i]);
 					break;
 
-				case 1: 
-					//var t = "Product2";
+				case 1:
+					//var t = "Pink couch";
 					document.querySelector('.cart-body').insertAdjacentHTML(
 			'afterbegin',
 			`<div class="row">
 			<input readonly class="product1"></input>
 			<input readonly class="amount1"></input>
 			</div>`)
-			$('.product1').val("Product2");
+			$('.product1').val("Pink couch");
 			$('.amount1').val(cart[i]);
 					break;
-		
+
 				case 2:
-					//var t = "Product3";
+					//var t = "Handmade cup";
 					document.querySelector('.cart-body').insertAdjacentHTML(
 			'afterbegin',
 			`<div class="row">
 			<input readonly class="product2"></input>
 			<input readonly class="amount2"></input>
 			</div>`)
-			$('.product2').val("Product3");
+			$('.product2').val("Handmade cup");
 			$('.amount2').val(cart[i]);
 					break;
-			
+
 				case 3:
-					//var t = "Product4";
+					//var t = "Pearl earrings heart";
 					document.querySelector('.cart-body').insertAdjacentHTML(
 			'afterbegin',
 			`<div class="row">
 			<input readonly class="product3"></input>
 			<input readonly class="amount3"></input>
 			</div>`)
-			$('.product3').val("Product4");
+			$('.product3').val("Pearl earrings heart");
 			$('.amount3').val(cart[i]);
 					break;
-			
-				case 4:
-					//var t = "Product5";
-					document.querySelector('.cart-body').insertAdjacentHTML(
-			'afterbegin',
-			`<div class="row">
-			<input readonly class="product4"></input>
-			<input readonly class="amount4"></input>
-			</div>`)
-			$('.product4').val("Product5");
-			$('.amount4').val(cart[i]);
-					break;
+
+                                case 4:
+                                        //var t = "My Dress-Up Darling";
+                                        document.querySelector('.cart-body').insertAdjacentHTML(
+                        'afterbegin',
+                        `<div class="row">
+                        <input readonly class="product4"></input>
+                        <input readonly class="amount4"></input>
+                        </div>`)
+                        $('.product4').val("Kitty cutlery set");
+                        $('.amount4').val(cart[i]);
+                                        break;
+
+
+                                case 5:
+                                        //var t = "Kitty cutlery set";
+                                        document.querySelector('.cart-body').insertAdjacentHTML(
+                        'afterbegin',
+                        `<div class="row">
+                        <input readonly class="product5"></input>
+                        <input readonly class="amount5"></input>
+                        </div>`)
+                        $('.product5').val("My Dress-Up Darling");
+                        $('.amount5').val(cart[i]);
+                                        break;
 
 				default:
-		
+
 				break;
 			}
 		//$('.product').val(t);
@@ -148,7 +164,7 @@ $(document).ready(function() {
 		}
 		}
 	}
-	
+
 	window.clearCart = function clearCart(){
 		for(let i=0; i<cart.length; i++){
 			cart[i]=0;
@@ -159,7 +175,7 @@ $(document).ready(function() {
 		$('.cart-body').html('');
 		addRow();
 	});
-	
+
 	$('.btn-order').on('click', function() {
 		//console.log(cart.length);
 		for(let i=0; i<cart.length; i++){
@@ -183,5 +199,5 @@ $(document).ready(function() {
 		}
 		}
 	});
-	
+
 });
